d344c356d71a7e3a2e001950baeb69e7c370a289
Gabriel <gabriel12domingosgaviolli@gmail.com>
2026-06-25 17:31:54 -0300

    25-06

--- src/services/usuarios/UsuariosServices.ts
+++ src/services/usuarios/UsuariosServices.ts
@@ -1,3 +1,8 @@
+import prismaClient from "../../Prisma/prismaClient";
+
+
+
+
 interface cadastrarUsuario {
     nome: string;
     email: string;
@@ -10,6 +15,28 @@ interface cadastrarUsuario {
 class UsuariosServices {
 
     async cadastrarUsuario({ nome, email, senha, telefone }: cadastrarUsuario) {
+
+        const emailExiste= await prismaClient.usuario.findFirst({
+            where:{
+                email: email
+            }
+
+        })
+        if(emailExiste){
+            throw new Error('E-mail ja cadastrado')
+        }
+
+        await prismaClient.usuario.create({
+            data: {
+                nome: nome,
+                email: email,            
+                senha: senha,
+                telefone: telefone
+            }
+        })
+
+        
+
         return ({ dados: "dados salvos com sucesso" })
 
     }
